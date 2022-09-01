f71a8253bfb6e92f32450d1ab015e2d17a865252
J0rdyV <28193980+J0rdyV@users.noreply.github.com>
2022-09-01 16:33:10 +0200

    Updates

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -6,18 +6,18 @@ c.downloads.position = 'bottom'
 c.editor.command = ['st', '-e', 'nvim', '{}']
 c.tabs.mousewheel_switching = False
 
-# c.fonts.default_family = 'Cascadia Code PL SemiLight'
 c.fonts.default_family = 'Terminus'
 c.fonts.default_size = '14px'
+
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.policy.page = 'always'
+c.colors.webpage.preferred_color_scheme = 'dark'
 
 c.tabs.padding = {"bottom": 2, "left": 5, "right": 5, "top": 2}
 c.statusbar.padding = {"bottom": 3, "left": 0, "right": 0, "top": 3}
 
 # c.spellcheck.languages = ['en-US']
 
-#c.url.searchengines['maps'] = 'https://www.google.com/maps?q=%s'
-
 c.aliases['ytdl'] = """spawn -v -m zsh -c 'cd ~/ && yt {url}'"""
 
-# config.source('gruvbox.py')
 config.source('blackwhite.py')
